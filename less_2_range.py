758a094ca6117afd9411bb9f9018c2127c0848cf
ElinaElissa <92169216+ElinaElissa@users.noreply.github.com>
2021-10-29 15:14:51 +0000

    Initial commit

---
+++ less_2_range.py
@@ -0,0 +1,16 @@
+'''
+по умолчанию: start = 0, step=1
+диапазон генеррирования: [start, stop)
+'''
+
+a = range(0, 10, 2)
+print(a)
+print(type(a))
+print(a[3])
+
+a = 'good'
+for i in range(0, 10, 1):
+  if i < len(a):
+    print(a[i] + ' - Bad')
+  else:
+    print(f'{i}' + ' - Good')
